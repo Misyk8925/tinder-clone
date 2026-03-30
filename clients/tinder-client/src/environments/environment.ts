1667c5d30582fde304054a474511a21bf4c43200
MykhailoKoval <20220450@htlstp.at>
2026-03-30 17:14:46 +0200

    edited frontend environment configuration

--- clients/tinder-client/src/environments/environment.ts
+++ clients/tinder-client/src/environments/environment.ts
@@ -1,4 +1,4 @@
-export const environment = {
+/*export const environment = {
   production: false,
   apiGatewayUrl: 'http://localhost:8222',
   wsUrl: 'ws://localhost:8222/ws',
@@ -7,4 +7,16 @@ export const environment = {
     realm: 'spring',
     clientId: 'test client'
   }
+};*/
+
+export const environment = {
+  production: true,
+  apiGatewayUrl: 'https://api.mykh.studio',
+  wsUrl: 'wss://api.mykh.studio/ws',
+  keycloak: {
+    url: 'https://auth.mykh.studio',
+    realm: 'spring',
+    clientId: 'tinder-client'
+  }
 };
+
