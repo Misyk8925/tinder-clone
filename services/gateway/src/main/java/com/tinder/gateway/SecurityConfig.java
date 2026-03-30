5ede7f044631cd53acf4b8c4ffb37e047337b4d1
MykhailoKoval <20220450@htlstp.at>
2026-03-30 17:39:58 +0200

    edited frontend environment configuration

--- services/gateway/src/main/java/com/tinder/gateway/SecurityConfig.java
+++ services/gateway/src/main/java/com/tinder/gateway/SecurityConfig.java
@@ -16,7 +16,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://tinder.mykh.studio"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
