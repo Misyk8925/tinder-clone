d62f8867096787ca0b4e91933b3dc0db1cde9768
MykhailoKoval <20220450@htlstp.at>
2026-08-20 19:59:58 +0200

    url fix

--- clients/tinder-client/src/environments/environment.prod.ts
+++ clients/tinder-client/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   designPreview: false,
-  apiGatewayUrl: 'https://api.misyk.tech',
-  wsUrl: 'wss://api.misyk.tech/ws',
+  apiGatewayUrl: 'https://matchapp.api.misyk.tech',
+  wsUrl: 'wss://matchapp.api.misyk.tech/ws',
   keycloak: {
     url: 'https://auth-app.misyk.tech',
     realm: 'spring',
